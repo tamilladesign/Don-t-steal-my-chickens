3ccd05aa68a5beacd1b8c8ce0383f582c6b38e4f
Tamilla <tamilla.muradova02@gmail.com>
2026-04-04 21:42:08 +0400

    text
    score text is added

--- Assets/Scripts/GetChicken.cs
+++ Assets/Scripts/GetChicken.cs
@@ -3,8 +3,9 @@ using UnityEngine;
 
 public class GetChicken : MonoBehaviour
 {
-    private float turningPoint = -1.5f;
+    private float turningPoint = -0.4f;
     public GameObject[] chickenPrefabs;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,10 +17,13 @@ public class GetChicken : MonoBehaviour
     void Update()
     {
         int chickenIndex = Random.Range(0, chickenPrefabs.Length);
+
+        
         if (transform.position.x < turningPoint)
         {
-            Instantiate(chickenPrefabs[chickenIndex], new Vector2(0, 0), chickenPrefabs[chickenIndex].transform.rotation);
-
+            Instantiate(chickenPrefabs[chickenIndex], transform.position, chickenPrefabs[chickenIndex].transform.rotation);
+            GameObject newChicken = Instantiate(chickenPrefabs[chickenIndex]);
+            newChicken.transform.SetParent(this.transform);
         }
     }
 }
